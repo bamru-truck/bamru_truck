6f7fdae4ec7a5f7a171f77d445ca925675307738
andyl <andy@r210.com>
2015-06-08 23:05:26 -0700

    updated web_admin

--- web_admin/app.rb
+++ web_admin/app.rb
@@ -1,5 +1,29 @@
 require 'sinatra'
 
-get '/' do
-  "Hello World"
+helpers do
+  def link_to_unless_current(path, label)
+    return label if path == request.path_info
+    "<a href='#{path}'>#{label}</a>"
+  end
+
+  def hdrs
+    %w(/:Home /erb:ERB_TEST /time:Time /ls:LS)
+  end
 end
+
+get '/' do
+  erb "Hello World"
+end
+
+get '/erb' do
+  erb :erb_test
+end
+
+get '/time' do
+  erb "Current Time: #{Time.now}"
+end
+
+get '/ls' do
+  erb `ls -1`.gsub("\n","<br/>")
+end
+
