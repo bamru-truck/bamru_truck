523b530da193b2fe95b8ce7421d32f1be4924077
andyl <andy@r210.com>
2015-09-21 16:27:43 -0700

    Refactor hgci
    - dashboard now handles attachment/reattachment
    - server and workers now kill prior instances (if they exist) and
      restart

--- hgci/lib/ci_server.rb
+++ hgci/lib/ci_server.rb
@@ -2,10 +2,14 @@ require 'sinatra/base'
 require 'json'
 require 'pp'
 
+$PROGRAM_NAME = "HGCI_SERVER"
+
 class CiServer < Sinatra::Base
 
-  BASE_DIR  = File.dirname(File.expand_path(__FILE__))
-  HIST_GLOB = BASE_DIR + "/history/*"
+  ROOT_DIR  = File.expand_path("#{File.dirname(__FILE__)}/../")
+  HIST_GLOB = ROOT_DIR + "/history/*"
+
+  set :root, ROOT_DIR
 
   # Accept Webhook posts for github, write contents to log/<sha>.json
   post '/event_handler' do
@@ -45,8 +49,8 @@ class CiServer < Sinatra::Base
   helpers do
     def queue_pull_request(pull_request)
       sha = pull_request['head']['sha']
-      system "mkdir -p #{BASE_DIR}/queue"
-      File.open("#{BASE_DIR}/queue/#{sha}.json", 'w') do |f|
+      system "mkdir -p #{ROOT_DIR}/queue"
+      File.open("#{ROOT_DIR}/queue/#{sha}.json", 'w') do |f|
         f.puts @json_pl
       end
     end
